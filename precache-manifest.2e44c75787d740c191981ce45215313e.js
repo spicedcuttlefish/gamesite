fab23b2ba43ddd7ddfcfed91e79f154cc048a601
Ringo <spicedcuttlefish@gmail.com>
2022-06-21 05:14:43 -0400

    Deploy React app to GitHub Pages

---
+++ precache-manifest.2e44c75787d740c191981ce45215313e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3283177726c1c43e542410baa06c8f54",
+    "url": "/gamesite/index.html"
+  },
+  {
+    "revision": "8a5187e21be7a4523a91",
+    "url": "/gamesite/static/css/main.7b7704d0.chunk.css"
+  },
+  {
+    "revision": "c3538bf25b2af412a268",
+    "url": "/gamesite/static/js/2.4f7c8eef.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/gamesite/static/js/2.4f7c8eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5187e21be7a4523a91",
+    "url": "/gamesite/static/js/main.49f99566.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/gamesite/static/js/main.49f99566.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ef121d86310d531d2a8",
+    "url": "/gamesite/static/js/runtime-main.49e9d2cf.js"
+  },
+  {
+    "revision": "de1a6e0267e44d5e4487922b774b2631",
+    "url": "/gamesite/static/media/assassinsmark.de1a6e02.webp"
+  },
+  {
+    "revision": "1dcc4698bd3a3439cd3c8de4d99c5847",
+    "url": "/gamesite/static/media/beamblade.1dcc4698.webp"
+  },
+  {
+    "revision": "c094b80b825f6f1172683c296376ac3a",
+    "url": "/gamesite/static/media/cryvalhalla.c094b80b.webp"
+  },
+  {
+    "revision": "357f7d3d199812b20f78174bfc12110b",
+    "url": "/gamesite/static/media/darkflare.357f7d3d.webp"
+  },
+  {
+    "revision": "678aa8d5652902b53aaf2f74244dccab",
+    "url": "/gamesite/static/media/deathstar.678aa8d5.webp"
+  },
+  {
+    "revision": "2973141af6f1b932396231def02c8354",
+    "url": "/gamesite/static/media/finalattack.2973141a.webp"
+  },
+  {
+    "revision": "c43ccccf41fa5b3546cd3cc4207cb881",
+    "url": "/gamesite/static/media/puncture.c43ccccf.webp"
+  },
+  {
+    "revision": "30b897984e83e3f0165c35096fa9f42b",
+    "url": "/gamesite/static/media/quadstar.30b89798.webp"
+  },
+  {
+    "revision": "891366e2b847267635c0f7b83f4b4ae6",
+    "url": "/gamesite/static/media/ragingblow.891366e2.webp"
+  },
+  {
+    "revision": "004a2007af7db00a98338536dfe396e5",
+    "url": "/gamesite/static/media/risingrage.004a2007.webp"
+  },
+  {
+    "revision": "fbfa411af54927a95470ad380c5179b2",
+    "url": "/gamesite/static/media/showdown.fbfa411a.webp"
+  },
+  {
+    "revision": "986eab495d74da177199167afe86d3ab",
+    "url": "/gamesite/static/media/suddenraid.986eab49.webp"
+  }
+]);
